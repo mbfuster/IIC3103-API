acc2c0deb5a77bfafb6712d25243823730cfbe17
Belen Fuster <mbfuster@uc.cl>
2020-03-31 02:17:14 -0300

    API ready

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,5 +1,15 @@
+require "http"
+require "json"
+
 class GreetingsController < ApplicationController
   def get_chapter
     @message = "Hello, how are you today?"
+    body = HTTP.get("https://rickandmortyapi.com/api/episode").body
+    results = JSON.parse(body)
+    @results = results["results"]
+    @length = @results.length
+
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'greetings/get_chapter'
+  root 'greetings#get_chapter'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
